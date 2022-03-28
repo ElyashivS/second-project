c29c37c817f2248f3e21d1327ee9e80273eaf130
Elyashiv <Elyashiv2468@gmail.com>
2022-03-28 16:34:30 +0300

    Some tests

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -3,8 +3,29 @@ describe('Ecommerce Application', () =>
     it('login page title', () =>
     {
         //webdriverIO code
-        browser.url("https://www.google.com/");
-        expect(browser).toHaveTitleContaining("Google");
-        // Test
-    });
+        browser.url("https://rahulshettyacademy.com/AutomationPractice/");
+        browser.maximizeWindow();
+        $("#radio-btn-example > fieldset > label:nth-child(3) > input").click();
+        $("#autocomplete").setValue("Israel");
+        $("#dropdown-class-example").selectByVisibleText("Option3");
+        browser.pause(2000);
+        $("#openwindow").click();
+        browser.switchWindow("http://www.qaclickacademy.com/");
+        browser.pause(2000);
+        browser.closeWindow();
+        browser.switchWindow("https://rahulshettyacademy.com/AutomationPractice/");
+        $("#opentab").click();
+        browser.pause(2000);
+        browser.switchWindow("https://rahulshettyacademy.com/AutomationPractice/");
+        $("#name").setValue("Dor Cohen");
+        $("#alertbtn").click();
+        browser.pause(2000);
+        browser.acceptAlert();
+        $("#mousehover").scrollIntoView();
+        $("#mousehover").moveTo();
+        browser.pause(2000);
+        $("body > div:nth-child(6) > div > fieldset > div > div > a:nth-child(2)").click();
+        browser.pause(2000);
+        expect(browser).toHaveTitle("Practice Page");
+        });
 });
