b873f37b15044d7a907716162f24357855f786da
Elyashiv <Elyashiv2468@gmail.com>
2022-03-30 16:32:09 +0300

    Second test

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -2,30 +2,40 @@ describe('Ecommerce Application', () =>
 {
     it('login page title', () =>
     {
-        //webdriverIO code
-        browser.url("https://rahulshettyacademy.com/AutomationPractice/");
+        // WebdriverIO code
+        browser.url("https://rahulshettyacademy.com/AutomationPractice/"); // Open website.
         browser.maximizeWindow();
-        $("#radio-btn-example > fieldset > label:nth-child(3) > input").click();
-        $("#autocomplete").setValue("Israel");
-        $("#dropdown-class-example").selectByVisibleText("Option3");
+
+        $("#radio-btn-example > fieldset > label:nth-child(3) > input").click(); // • Click the second radio button.
+        $("#autocomplete").setValue("Israel"); // •	On the suggestion class example – write “Israel”.
+        $("#dropdown-class-example").selectByVisibleText("Option3"); // • On the drop-down example – choose the last options.
         browser.pause(2000);
-        $("#openwindow").click();
+
+        // • Click on the switch window example and close it.
+        $("#openwindow").click(); 
         browser.switchWindow("http://www.qaclickacademy.com/");
         browser.pause(2000);
         browser.closeWindow();
+        
+        // • Click on the open tab - return to the previous one.
         browser.switchWindow("https://rahulshettyacademy.com/AutomationPractice/");
         $("#opentab").click();
         browser.pause(2000);
         browser.switchWindow("https://rahulshettyacademy.com/AutomationPractice/");
+
+        // • Send keys to the alert filed and click alert - close it.
         $("#name").setValue("Dor Cohen");
         $("#alertbtn").click();
         browser.pause(2000);
         browser.acceptAlert();
+
+        // • Scroll to Mouse Hover example and click reload - validate the tab title.
         $("#mousehover").scrollIntoView();
         $("#mousehover").moveTo();
         browser.pause(2000);
         $("body > div:nth-child(6) > div > fieldset > div > div > a:nth-child(2)").click();
         browser.pause(2000);
         expect(browser).toHaveTitle("Practice Page");
+
         });
 });
---
+++ test/specs/secondTest.js
@@ -0,0 +1,51 @@
+describe('Ecommerce Application', () =>
+{
+    it('login page title', () =>
+    {
+        // WebdriverIO code
+        // https://www.amazon.com/?language=en_US
+        browser.url("https://www.amazon.com/"); // Open website in English.
+        browser.maximizeWindow();
+
+        // Search an “apple pencil”.
+        $("#twotabsearchtextbox").setValue("apple pencil");
+        $("#nav-search-submit-button").click();
+
+        // Click on one of the options and add it to your cart.
+        // $("#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.sg-row > div.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(6) > div > div > div > div > div > div.sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20.s-list-col-right > div > div > div.a-section.a-spacing-none.s-padding-right-small.s-title-instructions-style > h2 > a > span").click();
+        browser.url("https://www.amazon.com/Stylus-Pen-Apple-iPad-Pencil/dp/B08JCKK9CC/ref=sr_1_4?keywords=apple+pencil&qid=1648645360&sprefix=apple+pen%2Caps%2C563&sr=8-4")
+        $("#add-to-cart-button").click();
+
+        // Validate that your cart has 1 item on the right top corner.
+        expect($("#nav-cart-count")).toHaveText("1");
+
+        // Search for “airpods 2nd generation” and click on of the options.
+        $("#twotabsearchtextbox").setValue("airpods 2nd generation");
+        $("#nav-search-submit-button").click();
+        // $("#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.sg-row > div.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(5) > div > div > div > div > div > div.sg-col.sg-col-4-of-12.sg-col-8-of-16.sg-col-12-of-20.s-list-col-right > div > div > div.a-section.a-spacing-none.s-padding-right-small.s-title-instructions-style > h2 > a > span").click();
+        browser.url("https://www.amazon.com/-/he/Apple-AirPods-%D7%A2%D7%9D-%D7%9B%D7%99%D7%A1%D7%95%D7%99-%D7%98%D7%A2%D7%99%D7%A0%D7%94/dp/B07SKLLYTW/ref=sr_1_3?keywords=airpods+2nd+generation&qid=1648645099&sr=8-3");
+
+
+        // Choose the quantity of 2 pieces and add them to the cart.
+        $("#a-autoid-0-announce").click();
+        $("#quantity_1").click();
+        $("#add-to-cart-button").click();
+        $("#attach-close_sideSheet-link").click();
+
+        // Validate that your cart has 3 items.
+        expect($("#nav-cart-count")).toHaveText("3");
+
+        // Sum the prices and validate the total amount.
+
+        // TODO: fix this
+        $("#nav-cart").click();
+
+        let firstItem = $("#sc-item-Caa0b3efc-0b7d-4561-8a22-1ba3000c3529 > div.sc-list-item-content > div > div.a-column.a-span2.a-text-right.sc-item-right-col.a-span-last > p > span").getText();
+        let secondItem = $("#sc-item-C3cbf4511-4534-42b0-99e4-a0a335131e5f > div.sc-list-item-content > div > div.a-column.a-span2.a-text-right.sc-item-right-col.a-span-last > p > span").getText();
+        // firstItem = String(firstItem).substring(1);
+        // secondItem = String(secondItem).substring(1);
+        // let sum = parseInt(firstItem) + parseInt(secondItem);
+        // console.log(sum);
+        
+        });
+});
